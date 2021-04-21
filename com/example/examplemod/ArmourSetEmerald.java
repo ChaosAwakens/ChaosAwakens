a118d96b05f7518a78d4cd995aba8babc9e241c7
Asbestosstar <ricsch97@gmail.com>
2021-04-21 16:14:47 +0100

    Add files via upload

---
+++ com/example/examplemod/ArmourSetEmerald.java
@@ -0,0 +1,17 @@
+package com.example.examplemod;
+
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ArmourSetEmerald extends ItemArmor {
+
+	public ArmourSetEmerald(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+		// TODO Auto-generated constructor stub
+	
+	this.setCreativeTab(getCreativeTab().COMBAT);
+	
+	
+	}
+
+}
---
+++ com/example/examplemod/EmeraldPic.java
@@ -0,0 +1,7 @@
+package com.example.examplemod;
+
+import net.minecraft.item.Item;
+
+public class EmeraldPic extends Item {
+
+}
