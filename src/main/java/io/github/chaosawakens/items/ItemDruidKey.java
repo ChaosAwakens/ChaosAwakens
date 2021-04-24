e8cb24f9c2ed61dc098a783ea8e69957b8de2f45
MemerTeamer <74426407+MemerTeamer@users.noreply.github.com>
2021-04-24 14:51:48 -0700

    Add files via upload

---
+++ src/main/java/io/github/chaosawakens/items/ItemDruidKey.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.chaosawakens.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.chaosawakens.ElementsChaosawakensMod;
+
+@ElementsChaosawakensMod.ModElement.Tag
+public class ItemDruidKey extends ElementsChaosawakensMod.ModElement {
+	@GameRegistry.ObjectHolder("chaosawakens:druid_key")
+	public static final Item block = null;
+	public ItemDruidKey(ElementsChaosawakensMod instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("chaosawakens:druid_key", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 1;
+			setUnlocalizedName("druid_key");
+			setRegistryName("druid_key");
+			setCreativeTab(CreativeTabs.MISC);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
