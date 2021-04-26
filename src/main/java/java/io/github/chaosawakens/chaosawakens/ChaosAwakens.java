44567e3da1b9ca793fe8baf3cb1e4d807a36b4a9
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2021-04-25 20:34:07 -0400

    clean slate for fabric

---
+++ src/main/java/java/io/github/chaosawakens/chaosawakens/ChaosAwakens.java
@@ -0,0 +1,10 @@
+package java.io.github.chaosawakens.chaosawakens;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChaosAwakens implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
