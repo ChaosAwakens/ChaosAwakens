97de7c074fc24d05aac97e707dca98d31f96aec5
invalid2 <weirdestnerd@gmail.com>
2021-06-07 09:15:31 -0300

    Fixed features part 2
    I'm just dumb, ok?

---
+++ src/main/java/io/github/chaosawakens/common/registry/CATags.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package io.github.chaosawakens.common.registry;
+
+import io.github.chaosawakens.ChaosAwakens;
+import net.minecraft.block.Block;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+
+/**
+ * @author invalid2
+ *
+ */
+public class CATags {
+	
+	public static final ITag.INamedTag<Block> BASE_STONE_CRYSTAL = BlockTags.makeWrapperTag(ChaosAwakens.MODID+":base_stone_crystal");
+}
