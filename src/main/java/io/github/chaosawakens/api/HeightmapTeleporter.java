6ed59bd1356ac89c554330185d4191cb6de12938
invalid2 <weirdestnerd@gmail.com>
2021-06-17 17:44:23 -0300

    Cleaned up HeightmapTeleporter code

--- src/main/java/io/github/chaosawakens/api/HeightmapTeleporter.java
+++ src/main/java/io/github/chaosawakens/api/HeightmapTeleporter.java
@@ -4,16 +4,14 @@ import java.util.function.Function;
 
 import javax.annotation.Nullable;
 
-import io.github.chaosawakens.ChaosAwakens;
 import net.minecraft.block.PortalInfo;
-import net.minecraft.block.PortalSize;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.TeleportationRepositioner;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.DimensionType;
+import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.util.ITeleporter;
@@ -26,44 +24,27 @@ public class HeightmapTeleporter implements ITeleporter {
 	
 	@Nullable
 	public PortalInfo getPortalInfo(Entity entity, ServerWorld targetWorld, Function<ServerWorld, PortalInfo> defaultPortalInfo) {
-		/*WorldBorder border = targetWorld.getWorldBorder();
-		double minX = Math.max(-2.9999872E7D, border.minX() + 16.0D);
-		double minZ = Math.max(-2.9999872E7D, border.minZ() + 16.0D);
-		double maxX = Math.min(2.9999872E7D, border.maxX() - 16.0D);
-		double maxZ = Math.min(2.9999872E7D, border.maxZ() - 16.0D);
+		WorldBorder border = targetWorld.getWorldBorder();
 		double coordDiff = DimensionType.getCoordinateDifference(entity.world.getDimensionType(), targetWorld.getDimensionType());
 		
 		//You know how walking a block on the nether equals to X in the overworld, this is checking for it
-		BlockPos targetPos = new BlockPos(MathHelper.clamp(entity.getPosX() * coordDiff * 1.0, minX, maxX), entity.getPosY(), MathHelper.clamp(entity.getPosZ() * coordDiff * 1.0, minZ, maxZ));
-		*/
+		Vector3d newPosVector = new Vector3d(
+				MathHelper.clamp(entity.getPosX() * coordDiff * 1.0, Math.max(-2.9999872E7D, border.minX() + 16.0D), Math.min(2.9999872E7D, border.maxX() - 16.0D)),
+				entity.getPosY(),
+				MathHelper.clamp(entity.getPosZ() * coordDiff * 1.0, Math.max(-2.9999872E7D, border.minZ() + 16.0D), Math.min(2.9999872E7D, border.maxZ() - 16.0D)));
+		
 		
 		BlockPos entityPos = entity.getPosition();
 		//Load target chunk
 		targetWorld.getChunk(entityPos);
 		
+		//Get a valid Y pos for the targeted block
 		BlockPos targetPos;
 		do {
-			targetPos = new BlockPos(entityPos.getX(), targetWorld.getHeight(Heightmap.Type.WORLD_SURFACE, entityPos.getX(), entityPos.getZ()), entityPos.getZ());
-		} while(targetPos.getZ() == 0);
+			targetPos = new BlockPos(newPosVector.getX(), targetWorld.getHeight(Heightmap.Type.WORLD_SURFACE, (int) newPosVector.getX(), (int) newPosVector.getZ()), newPosVector.getZ());
+		} while(targetPos.getY() == 0);
 		
-		Direction.Axis targetAxis;
-		if(MathHelper.abs(entity.rotationPitch) > MathHelper.abs(entity.rotationYaw)) {
-			targetAxis = Direction.Axis.X;
-		} else {
-			targetAxis = Direction.Axis.Z;
-		}
-		
-		//Move the player to the correct Y level
-		TeleportationRepositioner.Result result = TeleportationRepositioner.findLargestRectangle(targetPos, targetAxis, 2, Direction.Axis.Y, 120, (pos) -> {
-			return !(targetWorld.getBlockState(pos).isAir(targetWorld, pos) && !targetWorld.getBlockState(pos.down()).isAir(targetWorld, pos.down()));
-		});
-		
-		Vector3d motion = PortalSize.func_242973_a(result, targetAxis, entity.getPositionVec(), entity.getSize(entity.getPose()));
-		//The offset inside of the block an entity had before teleporting
-		Vector3d blockOffset = new Vector3d(entity.getPosX() - Math.floor(entity.getPosX()), 0, entity.getPosZ() - Math.floor(entity.getPosZ()));
-		ChaosAwakens.debug("TELEPORTER", blockOffset.getX()+" "+entity.getPosY()+" "+result.startPos+" "+result.height);
-		
-		return new PortalInfo(new Vector3d(targetPos.getX() + blockOffset.getX(), targetPos.getY(), targetPos.getZ() + blockOffset.getZ()), motion, entity.rotationYaw, entity.rotationPitch);
+		return new PortalInfo(new Vector3d(newPosVector.getX(), targetPos.getY(), newPosVector.getZ()), new Vector3d(0, 0, 0), entity.rotationYaw, entity.rotationPitch);
 	}
 	
 	@Override
