d6ff5764f7c52bdbc0ff8ff43c07d7a3f472c83f
invalid2 <weirdestnerd@gmail.com>
2021-09-01 19:43:54 -0300

    part 2

---
+++ src/main/java/io/github/chaosawakens/common/blocks/CropBodyPlantBlock.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package io.github.chaosawakens.common.blocks;
+
+import io.github.chaosawakens.ChaosAwakens;
+import net.minecraft.block.AbstractBodyPlantBlock;
+import net.minecraft.block.AbstractTopPlantBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
+
+/**
+ * @author invalid2
+ */
+public abstract class CropBodyPlantBlock extends AbstractBodyPlantBlock {
+	
+	/**
+	 * @param properties
+	 * @param direction
+	 * @param shape
+	 * @param p_i241179_4_
+	 */
+	public CropBodyPlantBlock(Properties properties, Direction direction, VoxelShape shape, boolean p_i241179_4_) {
+		super(properties, direction, shape, p_i241179_4_);
+	}
+	
+	public BlockState updateShape(BlockState state, Direction direction, BlockState state2, IWorld worldIn, BlockPos pos, BlockPos pos2) {
+		if (direction == this.growthDirection.getOpposite() && !state.canSurvive(worldIn, pos)) {
+			worldIn.getBlockTicks().scheduleTick(pos, this, 1);
+		}
+		
+		CropTopPlantBlock topBlock = (CropTopPlantBlock) this.getHeadBlock();
+		if (direction == this.growthDirection) {
+			Block block = state2.getBlock();
+			if (block != this && block != topBlock)
+				return topBlock.getUpdateShapeState(worldIn);
+		}
+		
+		if (this.scheduleFluidTicks)
+			worldIn.getLiquidTicks().scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
+		
+		return super.updateShape(state, direction, state2, worldIn, pos, pos2);
+	}
+	
+	@Override
+	public boolean canSurvive(BlockState state, IWorldReader reader, BlockPos pos) {
+		BlockPos downPos = pos.relative(this.growthDirection.getOpposite());
+		BlockState downState = reader.getBlockState(downPos);
+		Block block = downState.getBlock();
+		
+		//ChaosAwakens.debug("CORN-BODY", block+" "+downPos+" "+(block == this.getHeadBlock())+" "+(block == this.getBodyBlock())+" "+downState.is(Blocks.FARMLAND)+" "+state.isFaceSturdy(reader, pos, this.growthDirection));
+		return block == this.getHeadBlock() || block == this.getBodyBlock()
+			|| downState.is(Blocks.GRASS_BLOCK) || downState.is(Blocks.DIRT) || downState.is(Blocks.COARSE_DIRT) || downState.is(Blocks.PODZOL) || downState.is(Blocks.FARMLAND);
+	}
+	
+	@Override
+	protected abstract AbstractTopPlantBlock getHeadBlock();
+	
+}
