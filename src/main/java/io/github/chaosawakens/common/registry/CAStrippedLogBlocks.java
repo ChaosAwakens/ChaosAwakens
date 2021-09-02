e8a66672c2353a57f1bfc129ebe18f27ec028844
invalid2 <weirdestnerd@gmail.com>
2021-09-02 08:55:55 -0300

    Change to STRIPABLES, add logs tag to ca logs

--- src/main/java/io/github/chaosawakens/common/registry/CAStrippedLogBlocks.java
+++ src/main/java/io/github/chaosawakens/common/registry/CAStrippedLogBlocks.java
@@ -13,10 +13,10 @@ public class CAStrippedLogBlocks {
 
     //I went a bit crazy while making this because it took more than 15 minutes lmao
     public static void initveryfunni() {
-        registerCustomStrippedShit(CABlocks.DUPLICATION_LOG.get(), CABlocks.STRIPPED_DUPLICATION_LOG.get());
+        /*registerCustomStrippedShit(CABlocks.DUPLICATION_LOG.get(), CABlocks.STRIPPED_DUPLICATION_LOG.get());
         registerCustomStrippedShit(CABlocks.PEACH_LOG.get(), CABlocks.STRIPPED_PEACH_LOG.get());
         registerCustomStrippedShit(CABlocks.APPLE_LOG.get(), CABlocks.STRIPPED_APPLE_LOG.get());
-        registerCustomStrippedShit(CABlocks.CHERRY_LOG.get(), CABlocks.STRIPPED_CHERRY_LOG.get());
+        registerCustomStrippedShit(CABlocks.CHERRY_LOG.get(), CABlocks.STRIPPED_CHERRY_LOG.get());*/
     }
 
 
