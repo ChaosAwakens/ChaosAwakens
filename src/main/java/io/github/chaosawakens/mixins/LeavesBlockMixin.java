d5c2570daf6bfa4f79884a5b5c460c62e83ff6cf
invalid2 <weirdestnerd@gmail.com>
2023-02-11 17:26:24 -0300

    Updated Crimson, Oak and Warped Ent trees

--- src/main/java/io/github/chaosawakens/mixins/LeavesBlockMixin.java
+++ src/main/java/io/github/chaosawakens/mixins/LeavesBlockMixin.java
@@ -11,13 +11,18 @@ import net.minecraft.block.LeavesBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.server.ServerWorld;
 
+/**
+ * 
+ * This is here ONLY for testing foliage placers
+ *
+ */
 @Mixin(LeavesBlock.class)
 public abstract class LeavesBlockMixin {
 	
 	@Overwrite
 	public void randomTick(BlockState pState, ServerWorld pLevel, BlockPos pPos, Random pRandom) {
 		if (!pState.getValue(LeavesBlock.PERSISTENT) && pState.getValue(LeavesBlock.DISTANCE) == 7) {
-			pLevel.setBlock(pPos, Blocks.DIRT.defaultBlockState(), 0);
+			pLevel.setBlock(pPos, Blocks.DIRT.defaultBlockState(), 2);
 		}
 	}
 }
