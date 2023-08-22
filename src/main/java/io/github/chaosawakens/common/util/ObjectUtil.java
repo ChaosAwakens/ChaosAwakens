9a00b04b74f6a4a402e5b9c644f0dc9893ab1c0a
RaveTr <yuyuss210@gmail.com>
2023-08-22 19:45:58 +0200

    speliing err!!!

--- src/main/java/io/github/chaosawakens/common/util/ObjectUtil.java
+++ src/main/java/io/github/chaosawakens/common/util/ObjectUtil.java
@@ -15,7 +15,7 @@ public final class ObjectUtil {
 	 * Performs nullity checks, to ascertain that all object parameters passed in aren't null
 	 * @param throwsException Whether or not a {@link NullPointerException} should be thrown if any of the objects passed in are null
 	 * @param objects The objects to perform nullity checks on
-	 * @return True if all objects are not null, else returns False (or throws NPM if <code>throwsException</code> is true).
+	 * @return True if all objects are not null, else returns False (or throws NPE if <code>throwsException</code> is true).
 	 */
 	public static boolean performNullityChecks(boolean throwsException, Object... objects) {
 		for (Object obj : objects) {
