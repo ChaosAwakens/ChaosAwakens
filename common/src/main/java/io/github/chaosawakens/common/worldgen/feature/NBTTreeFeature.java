4aa7d242ae63e58ffc75561a28ed50ecb9756e2a
invalid2 <75389191+invalid2@users.noreply.github.com>
2025-08-04 12:26:06 -0300

    * Added processor list and random rotation on place to NBTTreeFeature

--- common/src/main/java/io/github/chaosawakens/common/worldgen/feature/NBTTreeFeature.java
+++ common/src/main/java/io/github/chaosawakens/common/worldgen/feature/NBTTreeFeature.java
@@ -1,7 +1,11 @@
 package io.github.chaosawakens.common.worldgen.feature;
 
 import com.mojang.serialization.Codec;
+import io.github.chaosawakens.CAConstants;
 import io.github.chaosawakens.common.worldgen.feature.configurations.NBTTreeConfiguration;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
@@ -22,13 +26,26 @@ public class NBTTreeFeature extends Feature<NBTTreeConfiguration> {
         NBTTreeConfiguration config = featurePlaceContext.config();
         Optional<StructureTemplate> treeTemplate = featurePlaceContext.level().getServer().getStructureManager().get(config.template().left().get());
 
-        if (treeTemplate.isPresent()) {
+        if (treeTemplate.isPresent() && config.validSurface().getState(featurePlaceContext.random(),featurePlaceContext.origin()) == featurePlaceContext.level().getBlockState(featurePlaceContext.origin())) {
             StructurePlaceSettings settings = new StructurePlaceSettings();
-
+            settings.setRotation(randomRotation(featurePlaceContext.random()));
             treeTemplate.get().placeInWorld(featurePlaceContext.level(), featurePlaceContext.origin(), featurePlaceContext.origin(), settings, featurePlaceContext.random(), 0);
             return true;
         }
 
         return false;
+    }
+
+    private Rotation randomRotation(RandomSource random) {
+        switch (random.nextInt(4)) {
+            case 0:
+                return Rotation.NONE;
+            case 1:
+                return Rotation.CLOCKWISE_90;
+            case 2:
+                return Rotation.CLOCKWISE_180;
+            default:
+                return Rotation.COUNTERCLOCKWISE_90;
+        }
     }
 }
--- common/src/main/java/io/github/chaosawakens/common/worldgen/feature/configurations/NBTTreeConfiguration.java
+++ common/src/main/java/io/github/chaosawakens/common/worldgen/feature/configurations/NBTTreeConfiguration.java
@@ -5,19 +5,24 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.ProcessorLists;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 
 import java.util.Optional;
 
-public record NBTTreeConfiguration(Either<ResourceLocation, StructureTemplate> template, BlockStateProvider validSurface, int groundLevel) implements FeatureConfiguration {
+public record NBTTreeConfiguration(Either<ResourceLocation, StructureTemplate> template, BlockStateProvider validSurface, int groundLevel, Optional<StructureProcessorList> list) implements FeatureConfiguration {
     public static final Codec<Either<ResourceLocation, StructureTemplate>> TEMPLATE_CODEC = Codec.of(NBTTreeConfiguration::encodeTemplate, ResourceLocation.CODEC.map(Either::left));
     public static final Codec<NBTTreeConfiguration> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             TEMPLATE_CODEC.fieldOf("location").forGetter(conf -> conf.template),
             BlockStateProvider.CODEC.fieldOf("valid_surface").forGetter(conf -> conf.validSurface),
-            Codec.intRange(0, 32).fieldOf("ground_level").forGetter(conf -> conf.groundLevel)
+            Codec.intRange(0, 32).fieldOf("ground_level").forGetter(conf -> conf.groundLevel),
+            StructureProcessorType.LIST_OBJECT_CODEC.optionalFieldOf("processors").forGetter(conf -> conf.list)
     ).apply(instance, NBTTreeConfiguration::new));
 
     private static <T> DataResult<T> encodeTemplate(Either<ResourceLocation, StructureTemplate> either, DynamicOps<T> ops, T templateObj) {
